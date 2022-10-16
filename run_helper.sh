cf1f6b4320a2007ffe4ad2d014ddce03b60ab6aa
Anthony Pernia <anthonyperniah@gmail.com>
2022-10-16 01:48:27 -0300

    solve problem pre-commit

---
+++ run_helper.sh
@@ -0,0 +1,5 @@
+python3 ./runner.py --bot-token 5708008068:AAGqvQa5R-YI6YFGkfRr5Q24HjnSYZDmNO4 --chat-id 1105713092
+
+#python3 ./runner.py --bot-token 5708008068:AAGqvQa5R-YI6YFGkfRr5Q24HjnSYZDmNO4 --chat-id 1105713092 --handle-messages 1
+
+##https://api.telegram.org/bot5708008068:AAGqvQa5R-YI6YFGkfRr5Q24HjnSYZDmNO4/getUpdates
